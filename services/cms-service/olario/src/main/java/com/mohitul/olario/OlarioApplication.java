65caa20a375622d5316f3579ac22cc7b3d583795
mohitul <dev.mohitul@gmail.com>
2026-05-26 16:30:55 +0600

    chore: add initial project structure with Maven wrapper and Spring Boot setup

---
+++ services/cms-service/olario/src/main/java/com/mohitul/olario/OlarioApplication.java
@@ -0,0 +1,13 @@
+package com.mohitul.olario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlarioApplication.class, args);
+	}
+
+}
---
+++ services/cms-service/olario/src/test/java/com/mohitul/olario/OlarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohitul.olario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
